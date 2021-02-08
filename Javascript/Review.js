74be15113f490214388cd3d3ef43b0d34a575930
Mark <markrdecello@gmail.com>
2021-02-08 17:04:02 -0500

    Creating my notes file

---
+++ Javascript/Review.js
@@ -0,0 +1,43 @@
+// This is an in-line comment in JavaScript 
+
+/* This is a
+multi-line comment in JavaScript*/
+
+//-------------------------------------------------------------------------------------------------------
+
+// Use the var keyword to create a variable called myName
+var myName;
+
+//-------------------------------------------------------------------------------------------------------
+
+// Assign the value 7 to variable a
+var a;
+
+a = 7;
+
+//-------------------------------------------------------------------------------------------------------
+
+// Assign the contents of a to variable b
+var a;
+a = 7;
+var b;
+
+b = a;
+
+//-------------------------------------------------------------------------------------------------------
+
+// Define a variable a with var and initialize it to a value of 9
+var a = 9;
+
+//-------------------------------------------------------------------------------------------------------
+
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1; // a should be defined and evaluated to have the value of 6.
+b = b + 5; // b should be defined and evaluated to have the value of 15.
+c = c + " String!"; // c should not contain undefined and should have a vlue of "I am a String!"
+
+//-------------------------------------------------------------------------------------------------------
