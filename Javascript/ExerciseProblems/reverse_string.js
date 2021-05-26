708bb77a7f8d64ae8bcb7ed3e41170d5940a8648
Mark <markrdecello@gmail.com>
2021-05-26 19:19:32 -0400

    Created a one liner code for reversing a string

---
+++ Javascript/ExerciseProblems/reverse_string.js
@@ -0,0 +1,10 @@
+/**
+ * Given a string, reverse it
+ * Example:
+ *  apple -> elppa
+ *  hello -> olleh
+ */
+let word = "Congratulations!"
+
+// This here is a one line code to reverse a string
+console.log(word.split("").reverse().join(""));
