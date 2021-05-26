57aadabff9f9afb044650e2f8ff02f11742c1b3e
Mark <markrdecello@gmail.com>
2021-05-26 19:22:15 -0400

    Added another without using libraries

--- Javascript/ExerciseProblems/reverse_string.js
+++ Javascript/ExerciseProblems/reverse_string.js
@@ -8,3 +8,10 @@ let word = "Congratulations!"
 
 // This here is a one line code to reverse a string
 console.log(word.split("").reverse().join(""));
+
+// Without using JavaScript libraries
+let reverseWord = "";
+for (let i of word) {
+    reverseWord = i + reverseWord;
+}
+console.log(reverseWord);
